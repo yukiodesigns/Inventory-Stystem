151566f7409d047d52cf9276c0843b81d2ac25d7
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-04-23 15:25:48 +0300

    Cretaed user auth routes

---
+++ routes/users.js
@@ -0,0 +1,64 @@
+const User = require('../models/user')
+const express = require('express')
+const router = express.Router()
+const SECRET_KEY = 'yukiisavibe'
+
+
+const authenticateUser = async(req, res, next)=>{
+    try{
+        const token = req.header('Authorization').replace('Bearer', ' ')
+        const decoded = jwt.verify(token, SECRET_KEY)
+        const user = await User.findOne({_id: decoded._id, 'tokens.token': token})
+        
+        if(!user){
+            throw new Error()
+        }
+        req.user = user
+        req.token = token
+        next()
+
+    }catch(err){
+        res.send({error: 'Authentication Failed'})
+    }
+}
+
+router.post('/', async(req, res)=>{
+    const {name, password, role} = req.body
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const user = new User({name, password:hashedPassword, role})
+    const token = jwt.sign({_id: user._id.toString()}, SECRET_KEY)
+    user.tokens = user.tokens.concat({token})
+    await user.save()
+    res.send({user, token}).status(201)
+})
+
+router.post('/login', async(req, res)=>{
+    try {
+        const {name, password} = req.body
+        const user = await User.findOne({name})
+
+        const isMatch = await bcrypt.compare(password, hashedPassword) 
+        if(!user || !isMatch){
+            throw new Error()
+        }
+        const token = jwt.sign({_id: user._id.toString()}, SECRET_KEY)
+        user.tokens = user.tokens.concat({token})
+        await user.save()
+        res.send({user, token})
+
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+router.post('/logout',authenticateUser, async(req, res)=>{
+    try {
+        req.user.tokens = req.user.tokens.filter((token)=> token.token !== req.token)
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+module.exports = router
