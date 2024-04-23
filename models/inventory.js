967dc3a9a9410834a3d8fef6f394634faa7fa3a7
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-04-23 15:02:59 +0300

    Updated inventory

--- models/inventory.js
+++ models/inventory.js
@@ -4,6 +4,7 @@ const inventorySchema = new mongoose.Schema({
     name: String,
     description: String,
     quantity: Number,
+    price: Number
 })
 
 const inventory = mongoose.model('Inventory', inventorySchema)
