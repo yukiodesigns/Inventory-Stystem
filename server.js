255da7a99a2ad8cde4bd92a5ca290cef1e138c88
Elsie Oduor <151344037+yukiodesigns@users.noreply.github.com>
2024-04-23 15:33:07 +0300

    Updated

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser =require('body-parser')
 const mongoose = require('mongoose')
+const inventories = require('./routes/inventories')
+const {Router} = require('./routes/users')
 
 const app = express()
 const PORT = 3000
@@ -8,6 +10,9 @@ const PORT = 3000
 app.use(bodyParser.json())
 mongoose.connect('mongodb://localhost:27017/inventory').then(()=> console.log('Connected to MongoDB'))
 .catch(err => console.log('Failed to connect', err))
+
+app.use('/inventory', inventories)
+app.use('/users', Router)
 
 app.get('/', (req, res)=>{
     res.send("Yuki's Inventory System")
